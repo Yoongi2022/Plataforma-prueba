f32be3b6cef54e9a1ef09ea7680c48ffd96a44a4
Yoongi2022 <108768440+Yoongi2022@users.noreply.github.com>
2023-12-29 17:05:02 -0600

    Add files via upload

---
+++ administrarclientes.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Administrar clientes</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="tabla">
+            <table>
+                <tr>
+                <th>ESTADO</th>
+                <th>CLIENTES</th>
+                <th>EMPRESA</th>
+                <th>OPCIONES</th>
+                </tr>   
+                <tr>
+                    <td>ACTIVO</td>
+                    <td>CARLOS EDUARDO FIGUEROA UMAÑA</td>
+                    <td>ROANSA</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>ACTIVO</td>
+                    <td>DESPACHOS ALTATA</td>
+                    <td>SPG</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>ACTIVO</td>
+                    <td>DISTRIBUIDORA INDUSTRIAL MEXICANA</td>
+                    <td>ROANSA</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>ACTIVO</td>
+                    <td>EXMIXTAPA S.A DE C.V.</td>
+                    <td>SPG</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>ACTIVO</td>
+                    <td>F.R. MEDICAL</td>
+                    <td>SPG</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>ACTIVO</td>
+                    <td>FRANCISCO JAVIER CABRAL VALDES</td>
+                    <td>SPG</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>ACTIVO</td>
+                    <td>INMOBILIARIA OHNO S.A DE C.V.</td>
+                    <td>SPG</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>ACTIVO</td>
+                    <td>INMOBILIARIA TENAZIT GAVILAN</td>
+                    <td>ROANSA</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>ACTIVO</td>
+                    <td>INMOBILIARIA TENAZIT ROJO GOMEZ</td>
+                    <td>ROANSA</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>ACTIVO</td>
+                    <td>INMOBILIARIA TENAZIT VICENTE GUERRERO</td>
+                    <td>ROANSA</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>ACTIVO</td>
+                    <td>MUNCHER S.A PROMOTORA DE INVERSIÓN DE CV</td>
+                    <td>SPG</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>ACTIVO</td>
+                    <td>ORGANIZACIÓN ORT</td>
+                    <td>SPG</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>ACTIVO</td>
+                    <td>POLYMERSHAPES MÉXICO BELLAVISTA</td>
+                    <td>ROANSA</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>ACTIVO</td>
+                    <td>POLYMERSHAPES MÉXICO IZTACALCO</td>
+                    <td>ROANSA</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>ACTIVO</td>
+                    <td>POLYMERSHAPES MÉXICO NAUCALPAN</td>
+                    <td>ROANSA</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>ACTIVO</td>
+                    <td>VORWER MÉXICO S DE RL DE CV</td>
+                    <td>ROANSA</td>
+                    <td>
+                    <a href="pagina2.html"> <img src="CarpetaIcono1.png"></a>
+                    <a href="pagina3.html"> <img src="UsuariosIcono3.png"></a>
+                    <input type="button" class="add2">
+                    <input type="button" class="add3">
+                </td>
+                </tr>
+                
+            </table>
+            
+            </form>
+        </div>
+    </body>
+</html>
